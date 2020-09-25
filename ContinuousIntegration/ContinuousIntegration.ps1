f09983a8c6c37b44cd89fe85a308204ea5f9bd0c
dfinke <finked@hotmail.com>
2020-09-25 19:33:36 -0400

    install modules

--- ContinuousIntegration/ContinuousIntegration.ps1
+++ ContinuousIntegration/ContinuousIntegration.ps1
@@ -1,1 +1,8 @@
-$PSVersionTable
+
+$modules = @("Pester", "ImportExcel")
+
+foreach ($module in $modules) {
+    Write-Host "Installing $module" -ForegroundColor Cyan
+    Install-Module $module -Force -SkipPublisherCheck
+    Import-Module $module -Force -PassThru
+}
