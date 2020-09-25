a7857baf53e336dc590429c35bf00e9b05ad232d
dfinke <finked@hotmail.com>
2020-09-25 19:47:57 -0400

    tweak tests

--- __tests__/sanity.tests.ps1
+++ __tests__/sanity.tests.ps1
@@ -1,5 +1,10 @@
 Describe "Just test" {
     It "Should be true" {
-        $false | Should -Be $true
+        if ($PSVersionTable.Platform -eq 'Unix') {
+            $true | Should -Be $true            
+        }
+        else {
+            $false | Should -Be $true
+        }
     }
 }
