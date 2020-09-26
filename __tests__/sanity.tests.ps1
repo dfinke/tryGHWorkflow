940d2b833a840fe165313a77f265132a10efb742
dfinke <finked@hotmail.com>
2020-09-26 06:04:19 -0400

    Added new tests

--- __tests__/sanity.tests.ps1
+++ __tests__/sanity.tests.ps1
@@ -1,11 +1,12 @@
 Describe "Just test" {
     It "Should be true" {
         $true | Should -Be $true            
-        # if ($PSVersionTable.Platform -eq 'Unix') {
-        #     $true | Should -Be $true            
-        # }
-        # else {
-        #     $false | Should -Be $true
-        # }
+    }
+
+    It "Test file created true" {
+        $fileName = "TestDrive:\test.txt"
+        "hello" > $fileName
+
+        Test-Path $fileName | should -be $true
     }
 }
